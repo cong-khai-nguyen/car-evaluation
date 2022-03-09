2152e193893d8d57d3c2ed91ebd1935d5126ec44
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-09 14:50:27 -0600

    final work

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ acc = model.score(x_test, y_test)
 predicted = model.predict(x_test)
 names = ["unacc", "acc", "good", "vgood"]
 
+# Accuracy : 95.375723%
 for x in range(len(predicted)):
     print("Predicted:", names[predicted[x]], "Data:", x_test[x], "Actual:", names[y_test[x]])
 
